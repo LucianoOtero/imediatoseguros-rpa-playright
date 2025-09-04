67b80caeb7da87658ae51e329913f7ddc89c6726
Luciano Otero <lrotero@gmail.com>
2025-09-04 14:49:28 -0300

    feat: Validação de veiculo_segurado atualizada v3.1.6 - Removido 'Nao' (sem acento) dos valores permitidos - Agora aceita apenas 'Sim' e 'Não' (com acento) - Padronização para uso correto de acentuação - Testado e funcionando corretamente

--- utils/validacao_parametros.py
+++ utils/validacao_parametros.py
@@ -72,7 +72,7 @@ class ValidadorParametros:
         # Valores permitidos para campos específicos
         self.valores_permitidos = {
             "combustivel": ["Flex", "Gasolina", "Álcool", "Diesel", "Híbrido", "Hibrido", "Elétrico"],
-            "veiculo_segurado": ["Sim", "Não", "Nao"],
+            "veiculo_segurado": ["Sim", "Não"],
             "sexo": ["Masculino", "Feminino"],
             "estado_civil": ["Solteiro", "Casado", "Divorciado", "Viuvo", "Uniao Estavel", "Casado ou Uniao Estavel", "Casado ou União Estável", "Separado"],
             "uso_veiculo": ["Pessoal", "Profissional", "Motorista de aplicativo", "Taxi"],
