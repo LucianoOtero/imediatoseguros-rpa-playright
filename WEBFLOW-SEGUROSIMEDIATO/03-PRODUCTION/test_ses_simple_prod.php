b7258dbd53ef4766bf9cd1c9f415f321006d6eff
Luciano Otero <lrotero@gmail.com>
2025-11-11 18:04:22 -0300

    Remover WEBFLOW-SEGUROSIMEDIATO: movido para repositório separado

--- WEBFLOW-SEGUROSIMEDIATO/03-PRODUCTION/test_ses_simple_prod.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'send_admin_notification_ses.php';
-echo \ Testando SES...\n\;
-\ = ['ddd' => '11', 'celular' => '987654321', 'nome' => 'Teste'];
-\ = enviarNotificacaoAdministradores(\);
-print_r(\);
